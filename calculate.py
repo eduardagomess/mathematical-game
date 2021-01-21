491153fb0a8b95b605176d729638f9dc054d1283
eduardagomessra <eduardaoliveiragomess@gmail.com>
2021-01-21 18:56:33 -0300

    it was modified the generate function

--- calculate.py
+++ calculate.py
@@ -5,10 +5,11 @@ class Calculate:
 
     def __init__(self, game_difficulty):
         self.__game_difficulty = game_difficulty
-        self.__value_one = self.generateValue
-        self.__value_two = self.generateValue
+        self.__value_one = self.generateValue()
+        self.__value_two = self.generateValue()
         self.__mathematical_operation = randint(1, 3)
-        self.__result = self.generateResult
+        self.__result = self.generateResult()
+        self.__math_operations = {1: ['Addition', '+'], 2: ['Subtraction', '-'], 3: ['Multiplication', '*'], 4: ['Division', '/']}
 
     @property
     def gameDifficulty(self):
@@ -32,15 +33,13 @@ class Calculate:
 
     def __str__(self):
         operation = ''
-        operations = {1: 'Addition', 2: 'Subtraction', 3: 'Multiplication', 4: 'Division'}
-        for num in operations:
+        for num in self.__math_operations:
             if num == self.__mathematical_operation:
-                operation = operations[num]
+                operation = self.__math_operations[num][0]
 
         return f' Value one: {self.__value_one} \n Value two: {self.__value_two} \n Game Difficulty: {self.__game_difficulty} \n' \
                f' Mathematical Operation: {operation}'
 
-    @property
     def generateValue(self):
         if self.__game_difficulty == 1:
             return randint(1, 10)
@@ -48,34 +47,23 @@ class Calculate:
             return randint(1, 100)
         return randint(1, 1000)
 
-    @property
     def symbolOfTheOperation(self):
-        if self.mathematicalOperation == 1:
-            return '+'
-        elif self.mathematicalOperation == 2:
-            return '-'
-        elif self.mathematicalOperation == 3:
-            return '*'
-        return '/'
+        return self.__math_operations[self.mathematicalOperation][1]
 
-    @property
     def generateResult(self):
-        if 1 == self.mathematicalOperation:
-            return self.__value_one + self.__value_two
-        elif 2 == self.mathematicalOperation:
-            return self.__value_one - self.__value_two
-        elif 3 == self.mathematicalOperation:
-            return self.__value_one * self.__value_two
-        return self.__value_one / self.__value_two
+        operations = {1: self.__value_one + self.__value_two, 2: self.__value_one - self.__value_two,
+                           3: self.__value_one * self.__value_two, 4: self.__value_one / self.__value_two}
+
+        return operations[self.mathematicalOperation]
 
     def verifyResult(self, player_answer):
-        if player_answer == self.generateResult:
+        if player_answer == self.generateResult():
             print("Right answer!")
             return True
         else:
             print("Wrong answer!")
-            print(f'{self.__value_one} {self.symbolOfTheOperation} {self.__value_two} = {self.__result}')
+            print(f'{self.__value_one} {self.__math_operations[self.mathematicalOperation][1]} {self.__value_two} = {self.result}')
             return False
 
     def operation(self):
-        print(f'{self.__value_one} {self.symbolOfTheOperation} {self.__value_two} = ?')
+        print(f'{self.__value_one} {self.__math_operations[self.mathematicalOperation][1]} {self.__value_two} = ?')
